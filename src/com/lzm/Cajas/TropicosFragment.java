6adedd39b4c0bd2690bac5ccf685ab1dd99de2ab
luzma87 <luzma_87@yahoo.com>
2014-10-01 12:21:04 -0500

    Tropicos API key

---
+++ src/com/lzm/Cajas/TropicosFragment.java
@@ -0,0 +1,38 @@
+package com.lzm.Cajas;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by luz on 01/10/14.
+ *
+ * Here is your API key:
+ * 34a6225b-552c-4e0b-9937-fe12a2541176
+ * Best wishes,
+ * Heather
+ * Missouri Botanical Garden
+ *
+ * Help is here:
+ * http://services.tropicos.org/help
+ * Example calls (using your api key):
+ * name search:
+ * http://services.tropicos.org/Name/Search?name=poa+annua&type=wildcard&apikey=34a6225b-552c-4e0b-9937-fe12a2541176&format=xml
+ *
+ * name detail:
+ * http://services.tropicos.org/Name/25509881?apikey=34a6225b-552c-4e0b-9937-fe12a2541176&format=xml
+ *
+ * synonyms:
+ * http://services.tropicos.org/Name/25509881/Synonyms?apikey=34a6225b-552c-4e0b-9937-fe12a2541176&format=xml
+ *
+ * accepted names:
+ * http://services.tropicos.org/Name/25503923/AcceptedNames?apikey=34a6225b-552c-4e0b-9937-fe12a2541176&format=xml
+ */
+public class TropicosFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return null;
+    }
+}
