be2fce76e2924b701714383f52ba2b84d03518fa
luz <luzma_87@yahoo.com>
2014-10-07 01:38:52 -0500

    indice en enciclopedia grid

--- src/com/lzm/Cajas/EnciclopediaGridFragment.java
+++ src/com/lzm/Cajas/EnciclopediaGridFragment.java
@@ -6,14 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import com.lzm.Cajas.adapters.EncyclopediaGridListAdapter;
 import com.lzm.Cajas.db.Especie;
 import com.lzm.Cajas.utils.Utils;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by DELL on 23/07/2014.
@@ -36,8 +36,10 @@ public class EnciclopediaGridFragment extends Fragment implements Button.OnClick
     String[] keysSorts;
 
     Integer sortPos = 0;
-
+    Map<String, Integer> mapIndex;
     EncyclopediaGridListAdapter adapter;
+
+    LinearLayout indexLayout;
 
     public EnciclopediaGridFragment() {
 
@@ -71,6 +73,7 @@ public class EnciclopediaGridFragment extends Fragment implements Button.OnClick
 
         View view = inflater.inflate(R.layout.encyclopedia_grid_layout, container, false);
 
+        indexLayout = (LinearLayout) view.findViewById(R.id.side_index);
 
         btnCambiarVista = (Button) view.findViewById(R.id.encyclopedia_grid_btn);
         btnCambiarVista.setOnClickListener(this);
@@ -95,6 +98,43 @@ public class EnciclopediaGridFragment extends Fragment implements Button.OnClick
         } else {
             adapter = new EncyclopediaGridListAdapter(context, especies);
             listView.setAdapter(adapter);
+        }
+        getIndexList(especies);
+        displayIndex();
+    }
+
+    private void getIndexList(List<Especie> especies) {
+        mapIndex = new LinkedHashMap<String, Integer>();
+        for (int i = 0; i < especies.size(); i++) {
+            String nombre;
+            if (sort.equalsIgnoreCase("f")) {
+                nombre = especies.get(i).familia;
+            } else if (sort.equalsIgnoreCase("n")) {
+                nombre = especies.get(i).genero;
+            } else {
+                nombre = especies.get(i).nombre;
+            }
+            String index = nombre.substring(0, 1);
+
+            if (mapIndex.get(index) == null) {
+                mapIndex.put(index, i);
+            }
+        }
+    }
+
+    private void displayIndex() {
+        List<String> indexList = new ArrayList<String>(mapIndex.keySet());
+        indexLayout.removeAllViews();
+        int totalH = context.screenHeight;
+        int totalItems = indexList.size();
+        int h = totalH / totalItems;
+        for (String index : indexList) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            TextView textView = (TextView) inflater.inflate(R.layout.encyclopedia_grid_side_index_item, null);
+            textView.setText(index);
+//            textView.setHeight(h);
+            textView.setOnClickListener(this);
+            indexLayout.addView(textView);
         }
     }
 
@@ -126,7 +166,9 @@ public class EnciclopediaGridFragment extends Fragment implements Button.OnClick
             btnSort.setText(sorts[sortPos]);
             sort = keysSorts[sortPos];
             loadData();
+        } else {
+            TextView selectedIndex = (TextView) view;
+            listView.setSelection(mapIndex.get(selectedIndex.getText()));
         }
-        System.out.println("ORDER: " + order + " SORT: " + sort);
     }
 }
